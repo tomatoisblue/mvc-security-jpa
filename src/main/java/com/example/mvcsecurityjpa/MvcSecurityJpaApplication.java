4a949b1ade19a5164a77523442828cc83c31149b
vahce <moto.by.handball@gmail.com>
2023-10-20 12:08:48 +0900

    initial commit

---
+++ src/main/java/com/example/mvcsecurityjpa/MvcSecurityJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.mvcsecurityjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcSecurityJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcSecurityJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mvcsecurityjpa/MvcSecurityJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mvcsecurityjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcSecurityJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
